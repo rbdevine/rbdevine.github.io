d36b02570e92ca4fa86591d338755db0eac5a18c
Rich Devine <rich@lark.com>
2019-08-04 23:40:18 -0400

    latest, not quite finished but out of time.

--- graphs.js
+++ graphs.js
@@ -1,7 +1,6 @@
-window.addEventListener( "load", makeC02Graph );
-window.addEventListener( "load", makeTempGraph );
+window.addEventListener( "load", loadFirstScene );
 document.addEventListener("DOMContentLoaded",function(e) {
-   var tabs = document.getElementsByClassName('tablinks');
+   var tabs = document.getElementsByClassName('scenelink');
    console.log("blah")
    console.log(tabs)
    console.log(tabs.length)
@@ -12,31 +11,101 @@ document.addEventListener("DOMContentLoaded",function(e) {
    }
 });
 
-async function makeC02Graph() {
-   const data = await d3.csv('energy-use-C02-world.csv')
-   const minYear = d3.min(data, function(d) { return d.Year; });
-   const maxYear = d3.max(data, function(d) { return d.Year; });
-   const minC02 = d3.min(data, function(d) { return d["CO2 (kt)"]; });
-   const maxC02 = d3.max(data, function(d) { return d["CO2 (kt)"]; });
-   //var margin = {top: 100, right: 100, bottom: 100, left: 100}
-   //, width = window.innerWidth - margin.left - margin.right // Use the window's width
-   //, height = window.innerHeight - margin.top - margin.bottom; // Use the window's height
-   const margin = 100
-   const width = 400
-   const height = 200
-   var svg = d3.select("#Scene1SVG")
-   
+async function makeTempGraph() {
+   console.log("makeTempGraph()")
+   const data = await d3.csv('epa-earth-temp.csv')
+   var minYear = d3.min(data, function(d) { return d.Year; });
+   var maxYear = d3.max(data, function(d) { return d.Year; });
+   var minC02 = d3.min(data, function(d) { return +d["Earth's surface (land and ocean)"]; });
+   var maxC02 = d3.max(data, function(d) { return +d["Earth's surface (land and ocean)"]; });
+   console.log(minC02)
+   console.log(maxC02)
+   var margin = {top: 50, right: 50, bottom: 50, left: 100}
+   var svg = d3.select("#Graph")
+   svg.attr("class","tempSVG")
+   const width = parseInt(svg.style("width")) - margin.top - margin.bottom
+   const height = parseInt(svg.style("height")) - margin.left - margin.right
    var x = d3.scaleLinear().domain([minYear,maxYear]).range([0,width])
    var y = d3.scaleLinear().domain([minC02,maxC02]).range([height,0])
 
-   //d3.select("#TokyoSVG").append("g").attr("class", "y axis")
    svg.append("g").attr("class", "y axis")
-      .attr("transform","translate("+margin+","+margin+")")
-      .call(d3.axisLeft(y).tickFormat(function(d){return d/1000000 }))
+      .attr("transform","translate("+(margin.left)+","+(margin.top)+")")
+      .call(d3.axisLeft(y).tickFormat(function(d){return d }))
 
    svg.append("g").attr("class", "x axis")
-      .attr("transform","translate("+margin+","+300+")")
+      .attr("transform","translate("+(margin.left)+","+((margin.top)+height)+")")
       .call(d3.axisBottom(x).tickFormat(d3.format("d")))
+
+   // text label for the y axis
+   svg.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", (margin.left/2))
+      .attr("x", -(margin.top + height/2))
+      .attr("dy", "1em")
+      .style("text-anchor", "middle")
+      .text("Earth's surface (land and ocean)");
+
+   // text label for the x axis
+   svg.append("text")             
+      .attr("transform",
+            "translate(" + (width/2 + margin.left) + " ," + 
+                           (height + margin.top + 40) + ")")
+      .style("text-anchor", "middle")
+      .text("Year");
+   
+   line = d3.line()
+      .x(function(d,i) { return x(d.Year); })
+      .y(function(d) { return y(d["Earth's surface (land and ocean)"]);})
+      .curve(d3.curveMonotoneX);
+
+   /* draw line */
+   svg.append("path")
+      .datum(data)
+      .attr("class", "line")
+      .attr("transform","translate("+(margin.left)+","+(margin.top)+")")
+      .attr("d", line);
+}
+
+async function makeGlacierGraph() {
+   const data = await d3.csv('epa-glacier-mass.csv')
+   const minYear = d3.min(data, function(d) { return d.Year; });
+   const maxYear = d3.max(data, function(d) { return d.Year; });
+   const minMass = d3.min(data, function(d) { return +d["Mean cumulative mass balance"]; });
+   const maxMass = d3.max(data, function(d) { return +d["Mean cumulative mass balance"]; });
+   const margin = {top: 50, right: 50, bottom: 50, left: 100}
+   //, width = window.innerWidth - margin.left - margin.right // Use the window's width
+   //, height = window.innerHeight - margin.top - margin.bottom; // Use the window's height
+   var svg = d3.select("#Graph")
+   svg.attr("class","glacierSVG")
+   const width = parseInt(svg.style("width")) - margin.top - margin.bottom
+   const height = parseInt(svg.style("height")) - margin.left - margin.right
+   var x = d3.scaleLinear().domain([minYear,maxYear]).range([0,width])
+   var y = d3.scaleLinear().domain([minMass,maxMass]).range([height,0])
+
+   svg.append("g").attr("class", "y axis")
+      .attr("transform","translate("+(margin.left)+","+(margin.top)+")")
+      .call(d3.axisLeft(y).tickFormat(function(d){return d }))
+
+   svg.append("g").attr("class", "x axis")
+      .attr("transform","translate("+(margin.left)+","+((margin.top)+height)+")")
+      .call(d3.axisBottom(x).tickFormat(d3.format("d")))
+
+   // text label for the y axis
+   svg.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", (margin.left/2 - 10))
+      .attr("x", -(margin.top + height/2))
+      .attr("dy", "1em")
+      .style("text-anchor", "middle")
+      .text("Mean cumulative mass balance");
+
+   // text label for the x axis
+   svg.append("text")             
+      .attr("transform",
+            "translate(" + (width/2 + margin.left) + " ," + 
+                           (height + margin.top + 40) + ")")
+      .style("text-anchor", "middle")
+      .text("Year");
 
    /*
    svg.append("g")
@@ -49,6 +118,196 @@ async function makeC02Graph() {
 
    line = d3.line()
       .x(function(d,i) { return x(d.Year); })
+      .y(function(d) { return y(d["Mean cumulative mass balance"]);})
+      .curve(d3.curveMonotoneX);
+
+   /* draw line */
+   svg.append("path")
+      .datum(data)
+      .attr("class", "line")
+      .attr("transform","translate("+(margin.left)+","+(margin.top)+")")
+      .attr("d", line);
+}
+
+async function makeSeaLevelGraph() {
+   const data = await d3.csv('epa-sea-level.csv')
+   const minYear = d3.min(data, function(d) { return d.Year; });
+   const maxYear = d3.max(data, function(d) { return d.Year; });
+   const minMass = d3.min(data, function(d) { return +d["CSIRO - Adjusted sea level (inches)"]; });
+   const maxMass = d3.max(data, function(d) { return +d["CSIRO - Adjusted sea level (inches)"]; });
+   const margin = {top: 50, right: 50, bottom: 50, left: 100}
+   //, width = window.innerWidth - margin.left - margin.right // Use the window's width
+   //, height = window.innerHeight - margin.top - margin.bottom; // Use the window's height
+   var svg = d3.select("#Graph")
+   svg.attr("class","glacierSVG")
+   const width = parseInt(svg.style("width")) - margin.top - margin.bottom
+   const height = parseInt(svg.style("height")) - margin.left - margin.right
+   var x = d3.scaleLinear().domain([minYear,maxYear]).range([0,width])
+   var y = d3.scaleLinear().domain([minMass,maxMass]).range([height,0])
+
+   svg.append("g").attr("class", "y axis")
+      .attr("transform","translate("+(margin.left)+","+(margin.top)+")")
+      .call(d3.axisLeft(y).tickFormat(function(d){return d }))
+
+   svg.append("g").attr("class", "x axis")
+      .attr("transform","translate("+(margin.left)+","+((margin.top)+height)+")")
+      .call(d3.axisBottom(x).tickFormat(d3.format("d")))
+
+   // text label for the y axis
+   svg.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", (margin.left/2 - 10))
+      .attr("x", -(margin.top + height/2))
+      .attr("dy", "1em")
+      .style("text-anchor", "middle")
+      .text("CSIRO - Adjusted sea level (inches)");
+
+   // text label for the x axis
+   svg.append("text")             
+      .attr("transform",
+            "translate(" + (width/2 + margin.left) + " ," + 
+                           (height + margin.top + 40) + ")")
+      .style("text-anchor", "middle")
+      .text("Year");
+
+   /*
+   svg.append("g")
+      .attr("transform","translate("+margin+","+margin+")")
+      .selectAll().data(data).enter().append("circle")
+      .attr("cx",function(d){return x(d.Year);})
+      .attr("cy",function(d){return y(d["CO2 (kt)"]);})
+      .attr("r",1)}
+*/
+
+   line = d3.line()
+      .x(function(d,i) { return x(d.Year); })
+      .y(function(d) { return y(d["CSIRO - Adjusted sea level (inches)"]);})
+      .curve(d3.curveMonotoneX);
+
+   /* draw line */
+   svg.append("path")
+      .datum(data)
+      .attr("class", "line")
+      .attr("transform","translate("+(margin.left)+","+(margin.top)+")")
+      .attr("d", line);
+}
+
+function loadFirstScene(e) {
+   openSelectedScene("Scene1")
+}
+
+function openSelectedScene(sceneName) {
+   console.log("loading " + sceneName )
+   var i, divs, links;
+
+   divs = document.getElementsByClassName("scenetext");
+   for (i = 0; i < divs.length; i++) {
+      divs[i].style.display = "none";
+   }
+
+   links = document.getElementsByClassName("scenelink");
+   for (i = 0; i < links.length; i++) {
+      links[i].className = links[i].className.replace(" active", "");
+   }
+
+   document.getElementById(sceneName+"Content").style.display = "block";
+   d3.selectAll("#Graph > *").remove()
+   document.getElementById(sceneName).className += " active";
+
+   switch(sceneName) {
+   case "Scene1":
+      makeC02Graph(false)
+      break;
+   case "Scene2":
+      makeC02Graph(true)
+      break;
+   case "Scene3":
+      makeTempGraph()
+      break;
+   case "Scene4":
+      makeGlacierGraph()
+      break;
+   case "Scene5":
+      makeSeaLevelGraph()
+      break;
+   case "Scene6":
+      break;
+   }
+}
+
+function openScene(e) {
+   const sceneName = e.currentTarget.id
+   openSelectedScene(sceneName)
+}
+
+async function makeC02Graph(annotate) {
+   const data = await d3.csv('energy-use-C02-world.csv')
+   const minYear = d3.min(data, function(d) { return d.Year; });
+   const maxYear = d3.max(data, function(d) { return d.Year; });
+   const minC02 = d3.min(data, function(d) { return d["CO2 (kt)"]; });
+   const maxC02 = d3.max(data, function(d) { return d["CO2 (kt)"]; });
+   const margin = {top: 50, right: 50, bottom: 50, left: 100}
+   //, width = window.innerWidth - margin.left - margin.right // Use the window's width
+   //, height = window.innerHeight - margin.top - margin.bottom; // Use the window's height
+   var svg = d3.select("#Graph")
+   svg.attr("class","co2SVG")
+   const width = parseInt(svg.style("width")) - margin.top - margin.bottom
+   const height = parseInt(svg.style("height")) - margin.left - margin.right
+   var x = d3.scaleLinear().domain([minYear,maxYear]).range([0,width])
+   var y = d3.scaleLinear().domain([minC02,maxC02]).range([height,0])
+
+   svg.append("g").attr("class", "y axis")
+      .attr("transform","translate("+(margin.left)+","+(margin.top)+")")
+      .call(d3.axisLeft(y).tickFormat(function(d){return d/1000000 }))
+
+   svg.append("g").attr("class", "x axis")
+      .attr("transform","translate("+(margin.left)+","+((margin.top)+height)+")")
+      .call(d3.axisBottom(x).tickFormat(d3.format("d")))
+   
+
+   // text label for the y axis
+   svg.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", (margin.left/2))
+      .attr("x", -(margin.top + height/2))
+      .attr("dy", "1em")
+      .style("text-anchor", "middle")
+      .text("Total CO2 Emissions (Gton)");
+
+   // text label for the x axis
+   svg.append("text")             
+      .attr("transform",
+            "translate(" + (width/2 + margin.left) + " ," + 
+                           (height + margin.top + 40) + ")")
+      .style("text-anchor", "middle")
+      .text("Year");
+
+   var div = d3.select("body").append("div")	
+       .attr("class", "tooltip")				
+       .style("opacity", 0);
+
+   svg.append("g")
+      .attr("transform","translate("+(margin.left)+","+(margin.top)+")")
+      .selectAll().data(data).enter().append("circle")
+      .attr("cx",function(d){return x(d.Year);})
+      .attr("cy",function(d){return y(d["CO2 (kt)"]);})
+      .attr("r",3)
+      .on("mouseover", function(d) {		
+	 div.transition()		
+	    .duration(200)		
+            .style("opacity", .9);
+	 div.html(parseInt(y(d["CO2 (kt)"])) + "</br>" + d.Year)	
+            .style("left", (d3.event.pageX) + "px")		
+            .style("top", (d3.event.pageY - 28) + "px");	
+      })					
+      .on("mouseout", function(d) {		
+         div.transition()		
+            .duration(500)		
+            .style("opacity", 0);	
+      });;
+   
+   line = d3.line()
+      .x(function(d,i) { return x(d.Year); })
       .y(function(d) { return y(d["CO2 (kt)"]);})
       .curve(d3.curveMonotoneX);
 
@@ -56,93 +315,58 @@ async function makeC02Graph() {
    svg.append("path")
       .datum(data)
       .attr("class", "line")
-      .attr("transform","translate("+margin+","+margin+")")
+      .attr("transform","translate("+(margin.left)+","+(margin.top)+")")
       .attr("d", line);
 
-   // Features of the annotation
-   const annotations = [
-      {
-	 note: {
-	    label: "Here is the annotation label",
-	    title: "Annotation title"
-	 },
-	 x: 200,
-	 y: 200,
-	 dy: 100,
-	 dx: 100
-      }
-   ]
+   if( annotate ) {
+      co2allowance = d3.line()
+	 .x(function(d,i) { return x(d.Year); })
+	 .y(function(d) { return y(17000000);})
+	 .curve(d3.curveMonotoneX);
 
-   // Add annotation to the chart
-   const makeAnnotations = d3.annotation()
-	 .annotations(annotations)
+      svg.append("path")
+	 .datum(data)
+	 .attr("class", "co2allowanceline")
+	 .attr("transform","translate("+(margin.left)+","+(margin.top)+")")
+	 .attr("d", co2allowance);
 
-   //d3.select("#General")
-   //svg.append("g")
-   //   .call(makeAnnotations)      
+      // Features of the annotation
+      const annotations = [
+	 /*{
+	    note: {
+	       label: "President Reagan's Deregulations",
+	       lineType:"none",
+	       orientation:"leftRight",
+	       "align":"middle"
+	    },
+	    type: d3.annotationCalloutCircle,
+	    subject: { radius: 15 },
+	    data: { x: 1983, y: 18484357 },
+	    dx: 40
+	 },*/
+	 {
+	    note: {
+	       label: "Nature's capacity to absorb CO2 exceeded",
+	       lineType:"none",
+	       orientation:"leftRight",
+	       "align":"top"
+	    },
+	    type: d3.annotationCalloutCircle,
+	    subject: { radius: 15 },
+	    data: { y: 17000000 },
+	    dy: -100
+	 }
+      ]
+
+      // Add annotation to the chart
+      const makeAnnotations = d3.annotation()
+	    .accessors({
+	       x: function(d){ return x(1975.5) + margin.left },
+	       y: function(d){ return y(d.y) + margin.top }
+	    })
+	    .annotations(annotations)
+    
+      d3.select("#Graph").append("g")
+	 .call(makeAnnotations)
+   }
 }
-
-async function makeTempGraph() {
-   console.log("makeTempGraph()")
-   const data = await d3.csv('epa-earth-temp.csv')
-   var minYear = d3.min(data, function(d) { return d.Year; });
-   var maxYear = d3.max(data, function(d) { return d.Year; });
-   var minC02 = d3.min(data, function(d) { return +d["Earth's surface (land and ocean)"]; });
-   var maxC02 = d3.max(data, function(d) { return +d["Earth's surface (land and ocean)"]; });
-   console.log(minC02)
-   console.log(maxC02)
-   //var margin = {top: 100, right: 100, bottom: 100, left: 100}
-   //, width = window.innerWidth - margin.left - margin.right // Use the window's width
-   //, height = window.innerHeight - margin.top - margin.bottom; // Use the window's height
-   margin = 100
-   var width = 400
-   var height = 200
-   var x = d3.scaleLinear().domain([minYear,maxYear]).range([0,width])
-   var y = d3.scaleLinear().domain([minC02,maxC02]).range([height,0])
-
-   var svg = d3.select("#Scene2SVG")
-
-   svg.append("g").attr("class", "y axis")
-      .attr("transform","translate("+margin+","+margin+")")
-      .call(d3.axisLeft(y).tickFormat(function(d){return d }))
-
-   svg.append("g").attr("class", "x axis")
-      .attr("transform","translate("+margin+","+300+")")
-      .call(d3.axisBottom(x).tickFormat(d3.format("d")))
-
-   line = d3.line()
-      .x(function(d,i) { return x(d.Year); })
-      .y(function(d) { return y(d["Earth's surface (land and ocean)"]);})
-      .curve(d3.curveMonotoneX);
-
-   /* draw line */
-   svg.append("path")
-      .datum(data)
-      .attr("class", "line")
-      .attr("transform","translate("+margin+","+margin+")")
-      .attr("d", line);
-}
-
-function openScene(e) {
-  const sceneName = e.currentTarget.id
-   //this.id
-  // Declare all variables
-  var i, tabcontent, tablinks;
-
-  // Get all elements with class="tabcontent" and hide them
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-
-  // Get all elements with class="tablinks" and remove the class "active"
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-
-  // Show the current tab, and add an "active" class to the button that opened the tab
-   document.getElementById(sceneName+"Content").style.display = "block";
-   e.currentTarget.className += " active";
-  //evt.currentTarget.className += " active";
-} 
